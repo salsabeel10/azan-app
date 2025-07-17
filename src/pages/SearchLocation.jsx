51061f463f70eeb664a4f3648d998b9ea39279bb
salsabeel10 <salsabeelibrahim10@gmail.com>
2025-07-17 17:27:38 +0400

    changed sbg icon

--- src/pages/SearchLocation.jsx
+++ src/pages/SearchLocation.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
-import InputField from '../components/InputField'
-import RadioButtons from '../components/RadioButtons'
-import AutoLocation from '../components/AutoLocation'
-import usePrayerTimeStore from '../store/usePrayerTimeStore'
-import Footer from '../components/Footer'
+import React from "react";
+import NavBar from "../components/NavBar";
+import InputField from "../components/InputField";
+import RadioButtons from "../components/RadioButtons";
+import AutoLocation from "../components/AutoLocation";
+import usePrayerTimeStore from "../store/usePrayerTimeStore";
+import Footer from "../components/Footer";
 
 const SearchLocation = () => {
-  const {locationType} = usePrayerTimeStore()
+  const { locationType } = usePrayerTimeStore();
   return (
-    <div className="flex flex-col bg-base-100 min-h-screen">
+    <div className="flex flex-col bg-base-300 min-h-screen">
       <NavBar />
 
-      <main className='flex-grow'>
-      <div className="flex flex-col items-center gap-5">
-        {/* Wrap InputField & RadioButtons */}
-        {locationType === 'manual' ? <InputField /> : <AutoLocation />}
-        <RadioButtons />
-      </div>
+      <main className="flex-grow">
+        <div className="flex flex-col items-center gap-5">
+          {/* Wrap InputField & RadioButtons */}
+          {locationType === "manual" ? <InputField /> : <AutoLocation />}
+          <RadioButtons />
+        </div>
       </main>
       <Footer />
-
     </div>
-  )
-}
+  );
+};
 
-export default SearchLocation
+export default SearchLocation;
